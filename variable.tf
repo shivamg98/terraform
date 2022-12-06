830a9c49228ccec0ddd5af58317efe1320d976fe
shivamg98 <92452528+shivamg98@users.noreply.github.com>
2022-12-07 01:00:11 +0530

    Update variable.tf
    update for app config

--- variable.tf
+++ variable.tf
@@ -1,19 +1,11 @@
-variable "virtual_machines" {
-  default     = {}
-  description = "Create a virtual machine from CAF module"
-}
+
 variable "resource_groups" {
   default = {}
 }
-variable "vnets" {
-  default = {}
-}
-variable "public_ip_addresses" {
-  default = {}
-}
-variable "keyvaults" {
-  default = {}
-}
+
 variable "global_settings" {
+  default = {}
+}
+variable "app_config" {
   default = {}
 }
