441f56973da0f2b73d867728f2f7febc2269c940
shivamg98 <92452528+shivamg98@users.noreply.github.com>
2022-12-07 00:57:50 +0530

    Update shared.tf
    update for app config

--- shared.tf
+++ shared.tf
@@ -4,14 +4,9 @@ module "caf" {
   
   global_settings = var.global_settings
   resource_groups = var.resource_groups
-  keyvaults       = var.keyvaults
-  logged_user_objectId = "f516f52d-54f2-41f0-85ee-27b8d7dc2d62"
-  compute = {
-    virtual_machines = var.virtual_machines
-  }
-
-  networking = {
-    public_ip_addresses = var.public_ip_addresses
-    vnets               = var.vnets
+  logged_user_objectId = "ba112f3f-f439-414a-aff9-505d0185120e"
+  
+  database = {
+    app_config                         = var.app_config
   }
 }
